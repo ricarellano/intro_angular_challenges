5d5acfd3ccd021c86ab966f7ea010c9df0672936
ricarellano <rarellanob@gmail.com>
2017-01-05 19:23:55 -0800

    challenges finished

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,28 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
-//
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME";
-// }
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("NewController", NewController);
+
+function WelcomeController(){
+  this.full_name = "Ricardo";
+  this.age = "35";
+  this.city = "Monterrey";
+  this.shout = function(){
+    return this.full_name.toUpperCase() +"?";
+  }
+
+}
+
+function NewController(){
+  this.class_name = "WDI";
+  this.enrolled_students = "11";
+  this.start_date = "11/28/16";
+  this.end_date = "03/03/15";
+  this.daysRemaining = function(){
+    var ms_left = Date.parse(this.end_date) - Date.now();
+   var days_left = Math.floor( ms_left/1000/24/60/60 );
+   return days_left;
+  }
+
+  }
